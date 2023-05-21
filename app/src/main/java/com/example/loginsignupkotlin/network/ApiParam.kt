fe9173ac2ee5b4110e643141a49d139db4db765e
Taghavi <80774755+TaghaviCe@users.noreply.github.com>
2023-05-21 10:13:33 +0330

    Update ApiParam.kt

--- app/src/main/java/com/example/loginsignupkotlin/network/ApiParam.kt
+++ app/src/main/java/com/example/loginsignupkotlin/network/ApiParam.kt
@@ -2,7 +2,7 @@ package com.example.loginsignupkotlin.network
 
 class ApiParam {
     // get your ip address and put it in url like below:
-    val BASE_URL = "http://192.168.1.8/retrofit/"
+    val BASE_URL = "http://192.168.X.X/retrofit/"
 
     //attention! -> your mobile or emulator and your pc connect to the same wifi
 }
